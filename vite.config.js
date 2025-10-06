d359812d12af15c95cc229c5d08b610a984fe917
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-06 18:21:20 +0100

    add to vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/afrifest-react-starter/',     // exact repo name
   plugins: [react()],
-});
-
+  build: { outDir: 'docs' }             // <= serve built files from /docs
+})
