73e2a7d7b6464932445184f9f9f060b7f87b49dc
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-06 18:10:24 +0100

    added updates

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,20 @@ import './styles/variables.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import PressRelease from "./components/PressRelease";
 import Contact from "./components/Contact";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 import About from './components/About.jsx';
 import FaqPage from './components/FaqPage.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/pressrelease" element={<PressRelease />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
       <Route path="/faq" element={<FaqPage />} />
-
-
-
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
