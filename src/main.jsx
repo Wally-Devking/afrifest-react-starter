59d590151fcb6472ecdd9e92916219464765b9ff
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-06 18:43:54 +0100

    update done

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/global.css'
-import './styles/variables.css'
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+import './styles/global.css';
+import './styles/variables.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import PressRelease from "./components/PressRelease";
 import Contact from "./components/Contact";
 import { HashRouter } from "react-router-dom";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import About from './components/About.jsx';
 import FaqPage from './components/FaqPage.jsx'; 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
     <Routes>
+      <App />
       <Route path="/" element={<App />} />
       <Route path="/pressrelease" element={<PressRelease />} />
       <Route path="/contact" element={<Contact />} />
@@ -23,4 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </HashRouter>
   </React.StrictMode>
 
-)
+    
+);
+
