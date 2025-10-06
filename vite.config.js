0df0a620f0a50931ff324670e0672c104aa3e3ed
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-06 18:43:14 +0100

    Build for GitHub Pages (docs) + base + HashRouter

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/afrifest-react-starter/',     // exact repo name
+  base: '/afrifest-react-starter/',   // EXACT repo name & casing
   plugins: [react()],
-  build: { outDir: 'docs' }             // <= serve built files from /docs
+  build: { outDir: 'docs' }           // Pages will serve /docs on main
 })
