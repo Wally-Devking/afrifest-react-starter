d3148139f13d69b364b559a71e533b35c935d568
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-06 18:56:24 +0100

    more updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,28 @@
-import ReactDOM from 'react-dom/client';
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
-import './styles/global.css';
-import './styles/variables.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./styles/global.css";
+import "./styles/variables.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import PressRelease from "./components/PressRelease";
 import Contact from "./components/Contact";
-import { HashRouter } from "react-router-dom";
-import { Routes, Route } from 'react-router-dom';
-import About from './components/About.jsx';
-import FaqPage from './components/FaqPage.jsx'; 
+import About from "./components/About.jsx";
+import FaqPage from "./components/FaqPage.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-    <Routes>
-      <App />
-      <Route path="/" element={<App />} />
-      <Route path="/pressrelease" element={<PressRelease />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/faq" element={<FaqPage />} />
-    </Routes>
-  </HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pressrelease" element={<PressRelease />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FaqPage />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
-
-    
 );
-
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/afrifest-react-starter/',   // EXACT repo name & casing
   plugins: [react()],
-  build: { outDir: 'docs' }           // Pages will serve /docs on main
+  build: { outDir: 'docs' }           // Pages serves /docs on main
 })
