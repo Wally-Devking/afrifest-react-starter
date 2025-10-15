d1ca014eac4024bac81ac8836b795119336e4c5e
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-15 23:32:20 +0100

    update fonts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/afrifest-react-starter/', // 👈 THIS LINE IS THE FIX
 })
