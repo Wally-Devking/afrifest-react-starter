36c6d91fb13d7886fba0dcc3343082d2a741c56c
Walter Omoruyi <omoruyiwally@gmail.com>
2025-10-16 09:11:21 +0100

    add images

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -11,7 +11,7 @@ export default function Sponsors(){
         </div>
         <div className="row justify-content-center align-items-center g-4">
           <div className="col-6 col-md-2 col-lg-2 text-center">
-            <img src="afrifest-react-starter/images/log9.jpg" alt="Sponsor 1" className="sponsor-logo img-fluid" />
+            <img src="/afrifest-react-starter/images/log9.jpg" alt="Sponsor 1" className="sponsor-logo img-fluid" />
           </div>
           <div className="col-6 col-md-2 col-lg-2 text-center">
             <img src="/afrifest-react-starter/images/mel.png" alt="Sponsor 2" className="sponsor-logo img-fluid" />
